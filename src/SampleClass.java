37adecac9fa97f4de1e356a0592f680dc22b73b6
Pallavi <pallavi.tathe30@gmail.com>
2026-02-20 16:26:44 +0530

    2nd commit

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -4,6 +4,8 @@ public class SampleClass {
 	public static void main(String[] args) {
 		
 		System.out.println("GitPractice 1");
+		System.out.println("GitPractice 2");
+		System.out.println("GitPractice 3");
 		
 	}
 
